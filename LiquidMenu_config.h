ad8eb51ab130acb62a5e6a4e85560514e98eab49
Futski-III <github@heili.us>
2018-04-11 19:37:31 -0500

    Add files via upload

---
+++ LiquidMenu_config.h
@@ -0,0 +1,32 @@
+/**
+@file
+Configuration file for LiquidMenu library.
+
+Contains global constants the configure the size of some of the arrays
+used in the library, also configures the debugging messages.
+*/
+
+#pragma once
+
+#ifndef I2C
+/// Switches the communication method to I2C
+#define I2C true ///< @note Default: false
+#endif
+
+/// Configures the number of available variables per line.
+const uint8_t MAX_VARIABLES = 5; ///< @note Default: 5
+
+/// Configures the number of available functions per line.
+const uint8_t MAX_FUNCTIONS = 8; ///< @note Default: 8
+
+/// Configures the number of available lines per screen.
+const uint8_t MAX_LINES = 12; ///< @note Default: 12
+
+/// Configures the number of available screens per menu.
+const uint8_t MAX_SCREENS = 14; ///< @note Default: 14
+
+/// Configures the number of available menus per menus system.
+const uint8_t MAX_MENUS = 8; ///< @note Default: 8
+
+// Turns the debugging messages on or off.
+#define LIQUIDMENU_DEBUG false ///< @note Default: false
